97d1cf9ce6670c0ef5bed693bf37b798f2ace14e
anton.voskobovich <anton.voskobovich@x5.ru>
2023-01-28 17:49:04 +0100

    initial personal projects

---
+++ evernote_analytics/notes.py
@@ -0,0 +1,37 @@
+
+from bs4 import BeautifulSoup
+
+import xml.etree.ElementTree as ET
+from io import StringIO
+
+
+def links(content):
+    soup = BeautifulSoup(content.replace('en-note', 'html').replace('\xa0', ''), "html.parser")
+    for div in soup.find_all('div'):
+        names = [c.name for c in div.children]
+        if 'a' in names:
+            texts = [c.text for c in div.children]
+            for a in [c for c in div.children if c.name == 'a']:
+                components = a.attrs['href'].split('/')
+                yield texts[0], [c for c in components if len(c) > 0][-1]
+
+
+def fix_link_names(note, notes):
+    root = ET.fromstring(note)
+    if root.text:
+        root = ET.fromstring(root.text.strip())
+
+    for a in root.findall('div/a'):
+        href = a.attrib['href']
+        target_note_id = href.split('/')[-2]
+        if target_note_id in notes:
+            a.text = notes[target_note_id]
+
+    x =  str(ET.tostring(root, xml_declaration=True, encoding='UTF-8'))
+    return f"""<content>
+      <![CDATA[<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">
+        {x}
+        ]]>
+    </content>
+        """
