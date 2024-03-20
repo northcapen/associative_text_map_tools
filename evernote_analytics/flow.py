2dc9a11c16baefc8f5030352e2741adb5b428897
anton.voskobovich <anton.voskobovich@x5.ru>
2024-03-20 07:59:38 +0100

    make relative from context_dir

--- evernote_analytics/flow.py
+++ evernote_analytics/flow.py
@@ -6,8 +6,8 @@ from prefect_shell import ShellOperation
 from notes_service import read_notes
 from utils import as_sqllite
 
-OUT_DB = 'data/en_small.db'
-OUT_DB_CORR = 'data/en_small_corr.db'
+OUT_DB = 'en_backup.db'
+OUT_DB_CORR = 'en_small_corr.db'
 
 @task
 def correct_links(db):
@@ -18,26 +18,28 @@ def correct_links(db):
     # process_incorrect_links(as_sqllite(OUT_DB_CORR))
 
 @task
-def export_enex(db, folder):
-    command = f"evernote-backup export -d {folder}/{db} --overwrite data/export2/"
+def export_enex(db, context_dir):
+    command = f"cd {context_dir} && evernote-backup export -d {db} --overwrite export/"
+    print(command)
     ShellOperation(commands=[command]).run()
 
 @task
-def yarle(folder, enex):
-    command = f" npx -p yarle-evernote-to-md@latest yarle yarle --configFile config.json"
+def yarle(context_dir, enex):
+    command = f"cd {context_dir} && npx -p yarle-evernote-to-md@latest yarle yarle --configFile ../config.json"
+    print(command)
     ShellOperation(commands=[command]).run()
 @task
-def db_to_parquet(folder):
-    df = read_notes(as_sqllite(OUT_DB))
-    df.to_parquet(f'{folder}/data/raw_notes')
+def db_to_parquet(context_dir):
+    df = read_notes(as_sqllite(context_dir + '/'+ OUT_DB))
+    df.to_parquet(f'{context_dir}/raw_notes')
 
 @flow
 def evernote_to_obsidian_flow():
-    folder = '.'
+    context_dir = 'small'
     #correct_links(db=OUT_DB)
-    export_enex(OUT_DB, folder)
-    yarle(folder, enex=None)
-    db_to_parquet(folder)
+    export_enex(OUT_DB, context_dir)
+    yarle(context_dir, enex=None)
+    db_to_parquet(context_dir)
 
 if __name__ == '__main__':
     evernote_to_obsidian_flow.serve('evernote-to-obsidian-flow')
