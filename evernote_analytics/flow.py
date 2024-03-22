002ba5344e04efcfdcaa92eebf45eed16e77b4df
anton.voskobovich <anton.voskobovich@x5.ru>
2024-03-22 17:25:27 +0100

    make flow more connected

--- evernote_analytics/flow.py
+++ evernote_analytics/flow.py
@@ -9,25 +9,32 @@ OUT_DB = 'en_backup.db'
 OUT_DB_CORR = 'en_backup_corr.db'
 
 @task
-def correct_links(db, context_dir):
+def correct_links(db: str, context_dir: str):
     from link_corrector import process_incorrect_links
 
     inclusive_filter = lambda nb: nb.name not in mostly_articles_notebooks
     exclusive_filter = lambda nb: nb.stack in ['Core', 'Maps']
+    #exclusive_filter = lambda nb: nb.name in ['Self']
 
 
     ShellOperation(commands=[f'cd {context_dir} && cp {db} {OUT_DB_CORR}']).run()
-    process_incorrect_links(as_sqllite(context_dir + '/' + OUT_DB_CORR), exclusive_filter)
+
+    out_db = as_sqllite(context_dir + '/' + OUT_DB_CORR)
+    out_db.execute('delete from notes')
+
+    process_incorrect_links(as_sqllite(context_dir + '/' + db), out_db, exclusive_filter)
+    return OUT_DB_CORR
 
 @task
-def export_enex(db, context_dir):
-    command = f"cd {context_dir} && evernote-backup export -d {db} --overwrite enex/"
+def export_enex(db, context_dir, target_dir):
+    command = f"cd {context_dir} && evernote-backup export -d {db} --overwrite {target_dir}/"
     print(command)
     ShellOperation(commands=[command]).run()
 
 @task
 def yarle(context_dir, enex):
-    command = f"cd {context_dir} && npx -p yarle-evernote-to-md@latest yarle yarle --configFile ../config.json"
+    ShellOperation(commands=[f'cp config.json {context_dir}/']).run()
+    command = f"cd {context_dir} && npx -p yarle-evernote-to-md@latest yarle yarle --configFile config.json"
     print(command)
     ShellOperation(commands=[command]).run()
 @task
@@ -37,9 +44,11 @@ def db_to_parquet(context_dir):
 
 @flow
 def evernote_to_obsidian_flow(context_dir):
-    correct_links(db=OUT_DB, context_dir=context_dir)
-    export_enex(OUT_DB_CORR, context_dir)
-    yarle(context_dir, enex=None)
+    #corr_db = correct_links(db=OUT_DB, context_dir=context_dir)
+
+    enex = 'enex'
+    #export_enex(db=corr_db, context_dir=context_dir, target_dir=enex)
+    yarle(context_dir, enex=enex)
     #db_to_parquet(context_dir)
 
 if __name__ == '__main__':
