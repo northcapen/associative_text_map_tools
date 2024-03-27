54cddbaa8cbdc53a8b2704d4827b50c6b36e9009
anton.voskobovich <anton.voskobovich@x5.ru>
2024-03-27 08:18:08 +0100

    fix tests

--- evernote2md/test_notes.py
+++ evernote2md/test_notes.py
@@ -3,11 +3,17 @@ from dataclasses import dataclass
 
 import xml.etree.ElementTree as ET
 
+from evernote2md.notes_service import NoteTO
+
+
 @dataclass
 class Note:
     title: str
     content :str = None
     notebook_name: str = 'A'
+
+def build_noteto(title, content=None):
+    return NoteTO(note=Note(title=title, content=content), notebook=None)
 
 def test_fix_links():
     content = """<content>
@@ -20,23 +26,23 @@ def test_fix_links():
     </content>"""
     p = LinkFixer()
     p.notes = {'eac75a87-f509-4eb3-a53c-9718cc6437d9' : Note(title='B_newname')}
-    x = p.transform(Note(title='B', content=content))
-    assert '>../A/B_newname<' in x.content
+    x = p.transform(build_noteto(title='B', content=content))
+    assert '>B_newname<' in x.content
 
 def test_evernote_link():
     x = """<a href="evernote:///view/9214951/s86/b/x/" style="color: #69aa35;">B</a>"""
     a = ET.fromstring(x)
-    assert 'B_newname' in canonicalize_evernote_link(a, {'b' : Note(title='B_newname')})
+    assert 'B_newname' in canonicalize_evernote_link(a, {'b' : build_noteto(title='B_newname')})
 
 
 def test_change_http_link():
     x = """<a href="https://www.smartsheet.com/" style="color: #69aa35;">B</a>"""
     a = ET.fromstring(x)
-    assert canonicalize_evernote_link(a, {'b' : Note(title='B_newname')}) is None
+    assert canonicalize_evernote_link(a, {'b' : build_noteto(title='B_newname')}) is None
 
 def test_evernote_with_html():
     content = """<en-note><div><a href="evernote:///view/9214951/s86/b/x/" rev="en_rl_none"><span style="color:#69aa35;">B</span></a></div></en-note>"""
     p = LinkFixer()
     p.notes = {'b': Note(title='B_newname')}
-    x = p.transform(Note(title='B', content=content))
-    assert '>../A/B_newname<' in x.content
+    x = p.transform(build_noteto(title='B', content=content))
+    assert '>B_newname<' in x.content
