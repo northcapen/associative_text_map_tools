596ba8ce2654ad70bb7ec4fd678032bc294ec8d9
anton.voskobovich <anton.voskobovich@x5.ru>
2024-04-12 08:53:07 +0200

    added typing

--- evernote2md/processors.py
+++ evernote2md/processors.py
@@ -1,13 +1,15 @@
+from typing import List
+
 from prefect import task
 
 from evernote2md.link_corrector import LinkFixer, ArticleCleaner
 from evernote2md.note_classifier import NoteClassifier
-from evernote2md.notes_service import note_metadata, deep_notes_iterator
+from evernote2md.notes_service import note_metadata, deep_notes_iterator, NoteTO
 from evernote2md.utils import as_sqllite
 from link_corrector import traverse_notes
 
 @task
-def clean_articles(context_dir, db, q):
+def clean_articles(context_dir, db, q) -> List[NoteTO]:
     indb = as_sqllite(context_dir + '/' + db)
     notes = list(deep_notes_iterator(indb, q))
 
@@ -15,7 +17,7 @@ def clean_articles(context_dir, db, q):
     return notes_cleaned
 
 @task
-def fix_links(context_dir, notes_cleaned):
+def fix_links(context_dir, notes_cleaned: List[NoteTO]) -> List[NoteTO]:
     from link_corrector import traverse_notes
 
     notes_p = note_metadata(context_dir, active=True)
@@ -26,6 +28,6 @@ def fix_links(context_dir, notes_cleaned):
     return traverse_notes(notes_cleaned, link_fixer)
 
 @task
-def enrich_data(links_fixed):
+def enrich_data(links_fixed: List[NoteTO]) -> List[NoteTO]:
     notes_classified = traverse_notes(notes=links_fixed, processor=NoteClassifier())
     return notes_classified
