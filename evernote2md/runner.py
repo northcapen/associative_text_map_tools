117517f4813ed23fae82966aed956b4bc0fbe27b
anton.voskobovich <anton.voskobovich@x5.ru>
2024-05-05 10:12:02 +0300

    support for running small in github actions

--- evernote2md/runner.py
+++ evernote2md/runner.py
@@ -1,7 +1,10 @@
 import logging
+import sys
 
 from evernote2md.flow import evernote_to_obsidian_flow
 
 logging.getLogger('evernote_backup').setLevel(logging.DEBUG)
 
-evernote_to_obsidian_flow(context_dir='data/small')
+ci_dir = sys.argv[1] if len(sys.argv) > 1 else 'small'
+
+evernote_to_obsidian_flow(context_dir='data/' + ci_dir)
