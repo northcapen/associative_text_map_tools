a97d3cc648bf9c452a61b9e5a1f6d94256186d3d
anton.voskobovich <anton.voskobovich@x5.ru>
2024-05-12 19:32:41 +0200

    initial graph ml version (backport from notebook)

--- graph/flow.py
+++ graph/flow.py
@@ -1,11 +1,11 @@
 import os
 
-from prefect import flow
+import networkx as nx
+from prefect import flow, task
 
 from evernote2md.tasks.source import read_notes_dataframe, read_links_dataframe
 
-
-@flow
+@task
 def build_cosma(context_dir):
     notes = read_notes_dataframe(context_dir)
     links = read_links_dataframe(context_dir)
@@ -15,3 +15,28 @@ def build_cosma(context_dir):
     os.makedirs(target, exist_ok=True)
     notes.to_csv(target + 'notes.csv', index=False)
     links.to_csv(target + 'links.csv', index=False)
+
+
+@task
+def build_graph_ml(context_dir, notes, links):
+    vertices = list(notes['id'])
+    edges = links[['from_guid', 'to_guid']]
+    edges = [(e[1], e[2]) for e in edges.itertuples()]
+
+    G = nx.MultiDiGraph()
+    G.add_nodes_from(vertices)
+    G.add_edges_from(edges)
+
+    connected_components = list(nx.connected_components(G.to_undirected()))
+    G_main = connected_components[6]
+    # a = sorted(list(nx.simple_cycles(G)), key=lambda s: len(s))
+    # nx.write_gexf(G, 'notes.gexf')
+    nx.write_graphml(G, 'notes_main.graphml')
+
+@flow
+def build_graph_stuff(context_dir):
+    notes = read_notes_dataframe(context_dir)
+    links = read_links_dataframe(context_dir)
+
+    build_graph_ml(context_dir, notes, links)
+    #build_cosma()
--- graph/runner.py
+++ graph/runner.py
@@ -1,11 +1,10 @@
 import logging
 import sys
 
-from evernote2md.flow import evernote_to_obsidian_flow
-from graph.flow import build_cosma
+from graph.flow import build_graph_stuff
 
 logging.getLogger('evernote_backup').setLevel(logging.DEBUG)
 
 ci_dir = sys.argv[1] if len(sys.argv) > 1 else 'full'
 
-build_cosma(context_dir='data/' + ci_dir)
+build_graph_stuff(context_dir='data/' + ci_dir)
