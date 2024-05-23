60db0fc34f92433d3c4916010be61f12ab772b65
anton.voskobovich <anton.voskobovich@x5.ru>
2024-05-23 19:16:14 +0200

    add db_to_pickle_flow as comment

--- evernote2md/runner.py
+++ evernote2md/runner.py
@@ -1,10 +1,11 @@
 import logging
 import sys
 
-from evernote2md.flow import evernote_to_obsidian_flow
+from evernote2md.flow import evernote_to_obsidian_flow, db_to_pickle_flow
 
 logging.getLogger('evernote_backup').setLevel(logging.DEBUG)
 
 ci_dir = sys.argv[1] if len(sys.argv) > 1 else 'small'
 
 evernote_to_obsidian_flow(context_dir='data/' + ci_dir)
+#db_to_pickle_flow(context_dir='data/' + ci_dir)
