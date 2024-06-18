2b63aa9d563d6d3332e400322e5fa31f98dd6402
anton.voskobovich <anton.voskobovich@x5.ru>
2024-06-18 23:50:33 +0400

    initial classifier

--- evernote2md/prepared/note_classifier.py
+++ evernote2md/prepared/note_classifier.py
@@ -3,6 +3,7 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
+from prefect import task
 
 from evernote2md.prepared.link_corrector import NoteTransformer
 from evernote2md.notes_service import NoteTO
@@ -11,24 +12,24 @@ from evernote2md.notes_service import NoteTO
 class NoteClassifier(NoteTransformer):
 
     def transform(self, note: NoteTO) -> Optional[NoteTO]:
-        pass
+        return note
 
 
-def notebook_classifier(context_dir):
-    notebooks_df = pd.read_parquet(context_dir + '/notebooks')
-    classify_notebook(notebooks_df)
+@task
+def categorise_notebooks(context_dir):
+    notebooks_df = pd.read_csv(context_dir + '/notebooks.csv')
+    categorise_notebooks0(notebooks_df)
+    notebooks_df.to_csv(f'{context_dir}/notebooks2.csv', index=False)
 
-    notebooks_df.to_csv('notebooks2.csv')
 
-
-def classify_notebook(notebooks: DataFrame):
-    techs = notebooks.stack == 'Techs'
-    objective = (techs) | (notebooks.name.str.contains('Ztk'))
+def categorise_notebooks0(notebooks: DataFrame):
+    techs = notebooks['stack'] == 'Techs'
+    objective = (techs) | (notebooks['name'].str.contains('Ztk'))
     notebooks['objective'] = np.where(objective, 'objective', 'subjective')
 
-    shortTerm = (notebooks.stack == 'Operations') | (notebooks.stack == 'Simple')
+    shortTerm = (notebooks['stack'] == 'Operations') | (notebooks['stack'] == 'Simple')
     notebooks['longetivity'] = np.where(shortTerm, 'short', 'long')
-    notebooks['authorship'] = np.where(notebooks.name.str.contains('Articles'), 'other', 'me')
+    notebooks['authorship'] = np.where(notebooks['name'].str.contains('Articles'), 'other', 'me')
 
     ex_work_projects = ['Segmento', 'Panda', 'Rainbow', 'Openway', 'HolyJS', 'Demand']
     pro = notebooks['stack'] == 'Professional'
@@ -37,17 +38,17 @@ def classify_notebook(notebooks: DataFrame):
     notebooks['it-specific'] = np.where(pro | pro_ex | pro_rubbles | techs, 'yes', 'no')
 
     # Combine conditions
-    more_external = notebooks.name.str.contains('LJ') | objective
+    more_external = notebooks['name'].str.contains('LJ') | objective
     notebooks['externality'] = np.where(more_external, 'more', 'less')
-    journals = notebooks.name == 'Dailys'
+    journals = notebooks['name'] == 'Dailys'
     notebooks['journals'] = np.where(journals, 'yes', 'no')
 
-    private = journals | (notebooks.name == 'Romance')
+    private = journals | (notebooks['name'] == 'Romance')
     confidential = pro_ex | pro_rubbles
     public = objective
 
     notebooks['sensitivity'] = np.where(private, 'private', np.where(confidential, 'confidential',
-                                                                     np.where(public, public,
+                                                                     np.where(public, 'public',
                                                                               'sensitive')))
 
 
