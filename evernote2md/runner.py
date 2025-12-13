7e27e51727d2609ecbd7df536074c82760d389a8
anton.voskobovich <avoskobovich@rubbles.ru>
2025-12-13 06:14:34 +0100

    fixed tests

--- evernote2md/runner.py
+++ evernote2md/runner.py
@@ -6,7 +6,7 @@ from evernote2md.flow import evernote_to_obsidian_flow, db_to_pickle_flow
 logging.getLogger('evernote_backup').setLevel(logging.INFO)
 
 ci_dir = sys.argv[1] if len(sys.argv) > 1 else 'small'
-run_db_to_pickle = sys.argv[2]  == 'db_to_pickle' if len(sys.argv) > 2 else 'skip'
+run_db_to_pickle = len(sys.argv) > 2 and sys.argv[2] == 'db_to_pickle'
 
 if run_db_to_pickle:
     db_to_pickle_flow(context_dir='data/' + ci_dir)
